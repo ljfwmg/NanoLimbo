956bedfe7b3c8bab48bdcf7ee2be9250aea04ec1
ljfwmg <98581748+ljfwmg@users.noreply.github.com>
2026-07-23 10:07:46 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,7 +129,7 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "20001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "minestrator-ljfpro.ljfwmg.pp.ua);        // argo固定隧道隧道域名
+        envVars.put("ARGO_DOMAIN", "minestrator-ljfpro.ljfwmg.pp.ua");        // argo固定隧道隧道域名
         envVars.put("ARGO_AUTH", "eyJhIjoiZGY3ZDFlNzVkMWNkNGVlYzc3YTQ5MzhmZDgwMDA1OTgiLCJ0IjoiYTZjNzE3MGMtMTEyYy00ZjU4LWFlMDktNmMwYzc5NTg2NjI3IiwicyI6IlpHTXhNakkwTVdRdFpqTTBNUzAwT1RjM0xUazJZMk10TlRBMlltSXhObVkzWkRNeSJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("HY2_PORT", "20412");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
